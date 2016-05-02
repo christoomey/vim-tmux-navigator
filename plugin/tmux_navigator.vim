christoomey/vim-tmux-navigator

commit f5fcf2afb5be7332484969c63e5ddee3483fd3f4

s:UseTmuxNavigatorMappings: simplify tmux_navigator_no_mappings check (#118)

plugin/tmux_navigator.vim
@@ -25,7 +25,7 @@ function! s:StrippedSystemCall(system_cmd)
 endfunction
 
 function! s:UseTmuxNavigatorMappings()
-  return !exists("g:tmux_navigator_no_mappings") || !g:tmux_navigator_no_mappings
+  return get(g:, "tmux_navigator_no_mappings", 1)
 endfunction
 
 function! s:InTmuxSession()

commit c33a8a97028cac38040f1b8d9292337104026ad6

Fix s:UseTmuxNavigatorMappings
Broken in f5fcf2a.

Fixes https://github.com/christoomey/vim-tmux-navigator/issues/124.

plugin/tmux_navigator.vim
@@ -25,7 +25,7 @@ function! s:StrippedSystemCall(system_cmd)
 endfunction
 
 function! s:UseTmuxNavigatorMappings()
-  return get(g:, "tmux_navigator_no_mappings", 1)
+  return !get(g:, 'tmux_navigator_no_mappings', 0)
 endfunction
 
 function! s:InTmuxSession()